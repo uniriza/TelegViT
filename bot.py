85be51dfc90a0c3e9a904148d5065063b9c3d7e3
ramaz <ramazanmoon228@gmail.com>
2023-01-29 21:29:26 +0300

    рабоча

--- bot.py
+++ bot.py
@@ -1,3 +1,10 @@
 import telebot
+from telebot import types
+token = '6034053467:AAFQKwgRvbTAYPdLRD4G1SVNmRThwadxnd8'
+client = telebot.TeleBot(token)
+@client.message_handler(content_types=['text'])
+def get_text(message):
+    client.send_message(message.chat.id, 'ska')
 
-bot = telebot.Telebot('6034053467:AAFQKwgRvbTAYPdLRD4G1SVNmRThwadxnd8')
+
+client.polling(non_stop=True)
---
+++ configure.py
@@ -0,0 +1,4 @@
+config = {
+    'name': 'ViTMassage',
+    'token': '6034053467:AAFQKwgRvbTAYPdLRD4G1SVNmRThwadxnd8'
+}
