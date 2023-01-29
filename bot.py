e65f001cde1eb47ec17a7a77eff8016568675871
ramaz <ramazanmoon228@gmail.com>
2023-01-29 22:05:23 +0300

    вставка кнопки

--- bot.py
+++ bot.py
@@ -1,10 +1,23 @@
 import telebot
 from telebot import types
+
+
+
+
 token = '6034053467:AAFQKwgRvbTAYPdLRD4G1SVNmRThwadxnd8'
 client = telebot.TeleBot(token)
+@client.message_handler(commands=['start'])
+def start(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    one_button = types.KeyboardButton("POSRAT")
+    markup.add(one_button)
+    client.send_message(message.chat.id, text="", reply_markup=markup)
 @client.message_handler(content_types=['text'])
-def get_text(message):
-    client.send_message(message.chat.id, 'ska')
+def func(message):
+    if(message.text == "POSRAT"):
+
+
+
 
 
 client.polling(non_stop=True)
